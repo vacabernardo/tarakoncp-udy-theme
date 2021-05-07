c739bae49b5fd66e6b4bc55d5c1bbd31df80ce82
vacabernardo <vacabernardo@users.noreply.github.com>
2021-05-07 11:12:04 -0500

    6.1

--- _utils/chunk.js
+++
@@ -1,7 +0,0 @@
-module.exports.chunk  = function(array, n){  
-    if (n == 0) {
-        return [array];
-    }
-  return Array(Math.ceil(array.length/n)).fill().map((_,i) => array.slice(i*n,i*n+n));
-}
-  
